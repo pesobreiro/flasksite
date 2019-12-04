86b078afbb63385e5f864910cfd6bb5123ea51aa
pesobreiro@gmail.com <pesobreiro@gmail.com>
2019-12-04 09:59:17 +0000

    changes

--- runserver.py
+++ runserver.py
@@ -7,11 +7,14 @@ We are testing the creation of the forms
 
 from os import environ
 from flasksite import app
+from flask_images import Images
 
 app.config['SECRET_KEY'] = 'you-will-never-guess'
 app.config['RECAPTCHA_PUBLIC_KEY'] = 'iubhiukfgjbkhfvgkdfm'
 app.config['RECAPTCHA_PARAMETERS'] = {'size': '100%'}
 app.debug = True
+
+images = Images(app)
 
 
 if __name__ == '__main__':
